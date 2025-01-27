869b65ccd95d506882373b17e074110249a6e2a5
OLEKSANDR KOVAL <koval.aleksandr.v@gmail.com>
2025-01-27 21:12:50 +0200

    3_modul_2

---
+++ GOiT/3_modul_2.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+
+def string_to_date(date_string):
+    return datetime.strptime(date_string, "%Y.%m.%d").date()
+
+
+def prepare_user_list(user_data):
+    prepared_users = []
+    for user in user_data:
+        prepared_user = {
+            "name": user["name"],
+            "birthday": string_to_date(user["birthday"])
+        }
+        prepared_users.append(prepared_user)
+    return prepared_users   
+    
+        
+users = [
+    {"name": "Bill Gates", "birthday": "1955.3.25"},
+    {"name": "Steve Jobs", "birthday": "1955.3.21"},
+    {"name": "Jinny Lee", "birthday": "1956.3.22"},
+    {"name": "John Doe", "birthday": "1985.01.23"},
+    {"name": "Jane Smith", "birthday": "1990.01.27"}
+]
+
+prepared_users = prepare_user_list(users)
+print(prepared_users)
