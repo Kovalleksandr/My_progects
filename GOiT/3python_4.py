df18ce7c607c69c3734084fb641907a34c67e972
OLEKSANDR KOVAL <koval.aleksandr.v@gmail.com>
2025-01-29 22:06:06 +0200

    3_modul_4

---
+++ GOiT/3python_4.py
@@ -0,0 +1,27 @@
+from datetime import datetime, date
+
+
+def string_to_date(date_string):
+    return datetime.strptime(date_string, "%Y.%m.%d").date()
+
+
+def date_to_string(date):
+    return date.strftime("%Y.%m.%d")
+
+
+def prepare_user_list(user_data):
+    prepared_list = []
+    for user in user_data:
+        prepared_list.append({"name": user["name"], "birthday": string_to_date(user["birthday"])})
+    return prepared_list
+
+
+def get_upcoming_birthdays(users, days=7):
+    upcoming_birthdays = []
+    today = date.today()
+    
+        
+        
+            
+            
+    return upcoming_birthdays
